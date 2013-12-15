0db76e38f20c84cdf38f529fe015e39f3c35b04f
Peter Szabo <pts@fazekas.hu>
2013-12-15 13:39:26 +0100

    unified clang_xstatic.c and clang_noxstatic.c to clang_trampoline.c

--- clang_trampoline.c
+++ clang_trampoline.c
@@ -1,14 +1,131 @@
 #define ALONE \
-    set -ex; exec ${CC:-gcc} -s -Os -fno-stack-protector \
-    -W -Wall -o clang_xstatic "$0"; exit 1
+    set -ex; ${CC:-gcc} -s -Os -fno-stack-protector \
+    -W -Wall -o clang_noxstatic "$0"; \
+    ${CC:-gcc} -s -Os -fno-stack-protector \
+    -W -Wall -DUSE_XSTATIC -o clang_xstatic "$0"; \
+    exit 0
 /*
- * clang_xstatic.c: clang trampoline for .so file redir and -static linking
+ * clang_trampline.c: clang trampoline for .so file redir and -static linking
  * by pts@fazekas.hu at Fri Dec 13 22:17:42 CET 2013
  *
  * Since our process is short-lived, we don't bother free()ing memory.
  */
 
-#include "clang_common.ci"
+#define _GNU_SOURCE 1  /* Needed for get_current_dir_name() */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+
+static char *strdupcat(const char *a, const char *b, const char *c) {
+  const size_t sa = strlen(a), sb = strlen(b), sc = strlen(c);
+  char *p = malloc(sa + sb + sc + 1);
+  memcpy(p, a, sa);
+  memcpy(p + sa, b, sb);
+  strcpy(p + sa + sb, c);
+  return p;
+}
+
+static char *readlink_alloc(const char *path) {
+  ssize_t len = 256;
+  ssize_t got;
+  char *buf = malloc(len);
+  for (;;) {
+    if (0 > (got = readlink(path, buf, len))) {
+      free(buf);
+      return NULL;
+    }
+    if (got < len) {
+      buf[got] = '\0';
+      return buf;
+    }
+    buf = realloc(buf, len <<= 1);
+  }
+}
+
+/** Resolve symlinks until a non-symlink is found. */
+static char *readlink_alloc_all(const char *path) {
+  char *path2, *path1 = strdup(path);
+  while ((path2 = readlink_alloc(path1))) {
+    if (path2[0] != '/') {
+      char *p;
+      for (p = path1 + strlen(path1); p != path1 && p[-1] != '/'; --p) {}
+      if (p != path1) {
+        *p = '\0';  /* Remove basename from path1. */
+        p = strdupcat(path1, "", path2);
+        free(path2);
+        path2 = p;
+      }
+    }
+    free(path1);
+    path1 = path2;
+  }
+  return path1;
+}
+
+static char *path_join(char *a, char *b) {
+  return !a ? b : (b[0] == '/') ? strdupcat("", "", b) : strdupcat(a, "/", b);
+}
+
+typedef enum archbit_t {
+  ARCHBIT_UNKNOWN = -1,
+  ARCHBIT_UNSPECIFIED = -2,
+  ARCHBIT_DETECTED = -3,
+  ARCHBIT_32 = 32,
+  ARCHBIT_64 = 64,
+} archbit_t;
+
+static archbit_t get_archbit_detected(char **argv) {
+  char **argi, *arg;
+  for (argi = argv + 1; (arg = *argi); ++argi) {
+    /* TODO(pts): Support Clang >3.3 flag `-arch i386'. */
+    /* Please note that -march=k8 doesn't force 64-bit compilation mode. */
+    /* Please note that we ignore -mcpu=, -march= and -mtune=, because neither
+     * Clang nor GCC autoswitch from -m32 to -m64 just by -march=k8.
+     */
+    /* gcc: `-mcpu=' is deprecated. Use `-mtune=' or '-march=' instead. */
+    if (0 == strcmp(arg, "-target") && argv[1]) {  /* Clang. */
+      /* -target=... doesn't work in Clang, so we don't match it. */
+      const char *p;
+      for (p = argv[1]; *p != '-' && *p != '\0'; ++p) {
+        /* Matches amd64 and x86_64. */
+        if (p[0] == '6' && p[1] == '4') return ARCHBIT_64;
+      }
+      return ARCHBIT_32;
+    } else if (0 == strcmp(arg, "-m32")) {
+      return ARCHBIT_32;
+    } else if (0 == strcmp(arg, "-m64")) {
+      return ARCHBIT_64;
+    }
+  }
+  return ARCHBIT_UNSPECIFIED;
+}
+
+static archbit_t get_archbit_override(archbit_t archbit_detected) {
+  struct utsname ut;
+  if (archbit_detected >= 0) return ARCHBIT_DETECTED;
+  if (uname(&ut) < 0) return ARCHBIT_DETECTED;
+  if (strstr(ut.machine, "64")) {  /* 64-bit kernel. */
+    /* Since nowadays there isn't anything useful in /lib directly, let's
+     * see if /bin/sh is a 64-bit ELF executable.
+     */
+    int fd = open("/bin/sh", O_RDONLY);
+    int got;
+    char buf[5];
+    if (fd < 0) return ARCHBIT_DETECTED;
+    if ((got = read(fd, buf, 5)) < 5) {
+      close(fd);
+      return ARCHBIT_DETECTED;
+    }
+    close(fd);
+    /* 64-bit ELF binary. */
+    if (0 == memcmp(buf, "\177ELF\002", 5)) return ARCHBIT_64;
+  }
+  return ARCHBIT_32;
+}
 
 int main(int argc, char **argv) {
   char *prog;
@@ -51,7 +168,9 @@ int main(int argc, char **argv) {
   /* It's important that we don't trigger when argv[1] is "-cc1", because
    * clang is calling itself in this case.
    */
-  if (argv[1] && 0 == strcmp(argv[1], "-xstatic")) {
+  if (0) {
+#ifdef USE_XSTATIC
+  } else if (argv[1] && 0 == strcmp(argv[1], "-xstatic")) {
     ++argv;
     --argc;
     /* When adding more arguments here, increase the args malloc count. */
@@ -74,6 +193,7 @@ int main(int argc, char **argv) {
     /* Run `clang -print-search-dirs' to confirm that it was properly
      * detected.
      */
+#endif
   } else if (!argv[1] || 0 != strcmp(argv[1], "-cc1")) {
     char need_linker = 1;
     char **argi, *arg, c;
